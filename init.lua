604aa685b5dd15674ebd66cd08a68be5941355c3
Marian Csontos <mcsontos@redhat.com>
2015-06-14 21:52:19 +0200

    Add pole signs

--- init.lua
+++ init.lua
@@ -50,9 +50,9 @@ local basic_properties = {
 	end,
 }
 
-local illuminated_properties = merge(basic_properties, {
-	light_source = 3
-})
+local illuminated_props = { light_source = 3 }
+
+local illuminated_properties = merge(basic_properties, illuminated_props)
 
 local colour_list = { "red", "blue", "green", "yellow", }
 
@@ -106,7 +106,7 @@ minetest.register_node("hiking:illuminated_"..colour.."_arrow_left", merge(illum
 }))
 
 minetest.register_craft({
-	output = "hiking:"..colour.."_sign",
+	output = "hiking:"..colour.."_sign 6",
 	recipe = {
 		{"dye:white"},
 		{"dye:"..colour},
@@ -115,9 +115,20 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = "hiking:"..colour.."_arrow_right",
+	output = "hiking:"..colour.."_arrow_right 6",
 	recipe = {
-		{"hiking:"..colour.."_sign","dye:"..colour}
+		{"hiking:"..colour.."_sign","hiking:"..colour.."_sign","",},
+		{"hiking:"..colour.."_sign","hiking:"..colour.."_sign","dye:"..colour,},
+		{"hiking:"..colour.."_sign","hiking:"..colour.."_sign","",},
+	}
+})
+
+minetest.register_craft({
+	output = "hiking:"..colour.."_arrow_left 6",
+	recipe = {
+		{"","hiking:"..colour.."_sign","hiking:"..colour.."_sign",},
+		{"dye:"..colour,"hiking:"..colour.."_sign","hiking:"..colour.."_sign",},
+		{"","hiking:"..colour.."_sign","hiking:"..colour.."_sign",},
 	}
 })
 
@@ -146,4 +157,118 @@ minetest.register_craft({
 	recipe = { "default:torch", "hiking:"..colour.."_arrow_right" }
 })
 
+end
+
+local hiking_pole_common = {
+	groups = {snappy=1, oddly_breakable_by_hand=2, },
+	--sounds = default.node_sound_stone_defaults(),
+	paramtype = "light",
+	sunlight_propagates = true,
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {{-0.03,-0.5,-0.03,0.03,0.5,0.03}},
+	},
+}
+local hiking_pole_bottom = merge(hiking_pole_common, {
+	 buildable_to = true,
+})
+local hiking_pole_top = merge(hiking_pole_common, {
+	 buildable_to = false,
+})
+
+local function mk_hiking_pole(id, name, top_face, moreprops, image)
+minetest.register_node("hiking:"..id.."_bottom", merge(merge(hiking_pole_bottom, {
+	-- TODO: this should be always on ground
+	description = name.." (bottom)",
+	tiles = {"hiking_pole_sign_cap.png", "hiking_pole_sign_cap.png", "hiking_pole_sign_bottom_.png", "hiking_pole_sign_bottom_.png", "hiking_pole_sign_bottom_.png", "hiking_pole_sign_bottom_.png", },
+	-- TODO: Add images:
+	inventory_image = image,
+	wield_image = image,
+
+	after_place_node = function(pos, placer, itemstack)
+		local node = minetest.env:get_node(pos)
+		local p = {x=pos.x, y=pos.y+1, z=pos.z}
+		node.name = "hiking:"..id.."_top"
+		if minetest.registered_nodes[minetest.env:get_node(p).name].buildable_to  then
+			minetest.env:set_node(p, node)
+		else
+			minetest.env:remove_node(pos)
+			return true
+		end
+	end,
+
+	after_destruct = function(pos, _)
+		local p = {x=pos.x, y=pos.y+1, z=pos.z}
+		if( minetest.env:get_node(p).name == "hiking:"..id.."_top" ) then
+			minetest.env:remove_node(p)
+		end
+	end,
+
+}), moreprops))
+
+minetest.register_node("hiking:"..id.."_top", merge(merge(hiking_pole_bottom, {
+	-- TODO: one should not build on top of this
+	-- TODO: should be always on top of pole_bottom
+	description = name.." (top)",
+	tiles = {"hiking_pole_sign_cap.png", "hiking_pole_sign_cap.png", top_face, top_face, top_face, top_face, },
+
+	on_dig = function(pos, _, _)
+		local p = {x=pos.x, y=pos.y-1, z=pos.z}
+		if( minetest.env:get_node(p).name == "hiking:"..id.."_bottom" ) then
+			minetest.env:remove_node(p)
+		end
+        end,
+}), moreprops))
+end
+
+minetest.register_craft({
+	output = "hiking:pole_bottom",
+	recipe = {
+		{"default:stick",},
+		{"default:stick",},
+	}
+})
+
+local function mk_hiking_pole_coloured(colour)
+	mk_hiking_pole(colour.."_pole", "Pole sign "..colour, "hiking_pole_sign_top_"..colour..".png", {}, "hiking_pole_sign_"..colour..".png")
+	minetest.register_craft({
+		output = "hiking:"..colour.."_pole_bottom",
+		recipe = {
+			{"hiking:"..colour.."_sign",},
+			{"default:stick",},
+			{"default:stick",},
+		}
+	})
+	minetest.register_craft({
+		output = "hiking:"..colour.."_pole_bottom",
+        	type = "shapeless",
+		recipe = { "hiking:pole_bottom", "hiking:"..colour.."_sign" }
+	})
+end
+
+local function mk_hiking_pole_illuminated_coloured(colour)
+	mk_hiking_pole("illuminated_"..colour.."_pole", "Illuminated pole sign "..colour, "hiking_pole_sign_top_"..colour..".png", illuminated_props, "hiking_illuminated_pole_sign_"..colour..".png")
+	minetest.register_craft({
+		output = "hiking:illuminated_"..colour.."_pole_bottom",
+        	type = "shapeless",
+		recipe = { "default:torch", "hiking:"..colour.."_pole_bottom" }
+	})
+	minetest.register_craft({
+		output = "hiking:illuminated_"..colour.."_pole_bottom",
+        	type = "shapeless",
+		recipe = { "hiking:pole_bottom", "hiking:illuminated_"..colour.."_sign" }
+	})
+	minetest.register_craft({
+		output = "hiking:illuminated_"..colour.."_pole_bottom",
+        	type = "shapeless",
+		recipe = { "hiking:illuminated_pole_bottom", "hiking:"..colour.."_sign" }
+	})
+end
+
+mk_hiking_pole("pole", "Pole sign", "hiking_pole_sign_bottom_.png", {}, "hiking_pole_sign.png")
+mk_hiking_pole("illuminated_pole", "Illuminated pole sign", "hiking_illuminated_pole_sign_bottom_.png", illuminated_props, "hiking_illuminated_pole_sign.png")
+for i, colour in ipairs(colour_list) do
+	mk_hiking_pole_coloured(colour)
+	mk_hiking_pole_illuminated_coloured(colour)
 end
